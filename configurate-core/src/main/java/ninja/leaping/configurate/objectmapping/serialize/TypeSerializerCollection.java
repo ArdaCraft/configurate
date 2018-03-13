e3a2b3ebf325c608ceea6993b6ac4a71fa4540e2
dags@dags.me <Tirril456>
2018-03-13 18:42:20 +0000

    knock back deps for 1.10

--- configurate-core/src/main/java/ninja/leaping/configurate/objectmapping/serialize/TypeSerializerCollection.java
+++ configurate-core/src/main/java/ninja/leaping/configurate/objectmapping/serialize/TypeSerializerCollection.java
@@ -42,7 +42,7 @@ public class TypeSerializerCollection {
         TypeSerializer<?> serial = typeMatches.get(type);
         if (serial == null) {
             for (Map.Entry<TypeToken<?>, TypeSerializer<?>> ent : typeMatches.entrySet()) {
-                if (ent.getKey().isSupertypeOf(type)) {
+                if (ent.getKey().isAssignableFrom(type)) {
                     serial = ent.getValue();
                     typeMatches.put(type, serial);
                     break;
